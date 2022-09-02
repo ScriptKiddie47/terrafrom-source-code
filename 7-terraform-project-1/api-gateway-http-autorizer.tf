15aaa0e7f9c0f97862866e7a60a5ee84db4bc700
ScriptKiddie47 <ritambala@live>
2022-09-02 18:30:12 +0530

    quick

--- 7-terraform-project-1/api-gateway-http-autorizer.tf
+++ 7-terraform-project-1/api-gateway-http-autorizer.tf
@@ -1,3 +1,43 @@
+data "aws_iam_policy_document" "apig_lambda_policy" {
+  statement {
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+    effect    = "Allow"
+    resources = [aws_lambda_function.authorizer-lambda.arn]
+    sid       = "ApiGatewayInvokeLambda"
+  }
+}
+
+
+data "aws_iam_policy_document" "apig_lambda_role_assume" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "apig_lambda_role" {
+  name               = "apigateway-authorize-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.apig_lambda_role_assume.json
+}
+
+resource "aws_iam_policy" "apig_lambda" {
+  name   = "apig-lambda-policy"
+  policy = data.aws_iam_policy_document.apig_lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "apig_lambda_role_to_policy" {
+  role       = aws_iam_role.apig_lambda_role.name
+  policy_arn = aws_iam_policy.apig_lambda.arn
+}
+
 resource "aws_apigatewayv2_authorizer" "example" {
   authorizer_payload_format_version = "2.0"
   api_id                            = aws_apigatewayv2_api.company-connect-http-api-gw.id
@@ -7,5 +47,5 @@ resource "aws_apigatewayv2_authorizer" "example" {
   name                              = "company-authorizer"
   authorizer_uri                    = aws_lambda_function.authorizer-lambda.invoke_arn
   enable_simple_responses           = true
-  authorizer_credentials_arn        = aws_iam_role.authorizer-lambda-iam-role.arn 
+  authorizer_credentials_arn        = aws_iam_role.apig_lambda_role.arn
 }
