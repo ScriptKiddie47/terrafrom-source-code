5cacda6ee5724661dca9bb414085e616a1fd84fc
S Bala <59485946+ScriptKiddie47@users.noreply.github.com>
2022-10-25 17:22:55 +0530

    Update company-data-fetcher.js

--- 9-terrafom-vpc-lambda-dynamo-auth/node-source/fetcher_code/company-data-fetcher.js
+++ 9-terrafom-vpc-lambda-dynamo-auth/node-source/fetcher_code/company-data-fetcher.js
@@ -1,9 +1,11 @@
 const aws = require("aws-sdk");
 const dynamo = new aws.DynamoDB.DocumentClient({ region: "ap-south-1" });
 exports.handler = async (event, context, callback) => {
+    let responseList;
     await readMessage()
         .then((data) => {
-            console.log(data)
+            console.log(data);
+            responseList = data.Items;
             data.Items.forEach((item) => {
                 console.log(item.ST_CD);
             });
@@ -11,6 +13,11 @@ exports.handler = async (event, context, callback) => {
         .catch((err) => {
             console.error(err);
         });
+    const responseMsg = {
+        statusCode : 200,
+        body:JSON.stringify(responseList)
+    };
+    return responseMsg;
 };
 
 function readMessage() {
